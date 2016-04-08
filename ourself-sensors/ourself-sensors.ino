9d335f1301087f9f57c54c96edde0d63b607b821
Brandon Stafford <brandon@pingswept.org>
2016-04-08 17:10:41 -0400

    Make light bar respond to sensor via DMX; trigger audio

--- ourself-sensors/ourself-sensors.ino
+++ ourself-sensors/ourself-sensors.ino
@@ -16,3 +16,53 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
+#include <DmxSimple.h>
+
+#define USONIC_THRESHOLD     100
+#define THE_ONLY_BAR_WE_HAVE 1
+
+#define DMX_OUT 6
+
+#define PIR_R_OUT  9
+#define USONIC_OUT 10
+#define PIR_L_OUT  11
+
+#define PIR_L_IN  14
+#define USONIC_IN A4
+#define PIR_R_IN  22
+
+void setup() {
+    pinMode(DMX_OUT, OUTPUT);    // send DMX
+    pinMode(PIR_R_OUT, OUTPUT);  // announce PIR R signal
+    pinMode(USONIC_OUT, OUTPUT); // announce USONIC signal
+    pinMode(PIR_L_OUT, OUTPUT);  // announce PIR L signal
+    pinMode(PIR_L_IN, INPUT);    // read PIR L
+    pinMode(PIR_R_IN, INPUT);    // read PIR R
+    Serial.begin(115200);
+    DmxSimple.usePin(6);
+    DmxSimple.maxChannel(100);
+}
+
+void setLightBar(int address, int r, int g, int b) {
+    DmxSimple.write(address, 255);   // master dimmer, 0-255 -> dim-bright
+    DmxSimple.write(address + 1, 0); // flash rate: 0 = no flash, 1-255 -> long-short delay between flashes
+    DmxSimple.write(address + 2, 0); // whether to run some fucked up program. 0-50=no, other numbers run weird shit
+    DmxSimple.write(address + 3, 0); // how fast to fade in weird shit, 0-255 -> slow-fast
+    DmxSimple.write(address + 4, r); // red channel, 0-255 -> dim-bright
+    DmxSimple.write(address + 5, g); // green channel, 0-255 -> dim-bright
+    DmxSimple.write(address + 6, b); // blue channel, 0-255 -> dim-bright
+}
+
+void loop() {
+    int i;
+
+    int sensorValue = analogRead(USONIC_IN);
+    if(sensorValue < USONIC_THRESHOLD) {
+        digitalWrite(USONIC_OUT, HIGH);
+    } else {
+        digitalWrite(USONIC_OUT, LOW);
+    }
+    Serial.println(sensorValue);
+    delay(100);
+    setLightBar(THE_ONLY_BAR_WE_HAVE, 255 - min(sensorValue, 255), 0, min(sensorValue, 255));
+}

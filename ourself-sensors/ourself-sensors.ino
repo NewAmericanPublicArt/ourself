48635caa023cb3bf24a40c9984829f2e99981deb
pingswept <someone@somewhere.gov>
2016-04-15 21:59:38 -0400

    Add state machine

--- ourself-sensors/ourself-sensors.ino
+++ ourself-sensors/ourself-sensors.ino
@@ -18,6 +18,9 @@
 
 #include <DmxSimple.h>
 
+#define TRUE  1
+#define FALSE 0
+
 #define USONIC_THRESHOLD     100
 #define THE_ONLY_BAR_WE_HAVE 1
 
@@ -31,17 +34,13 @@
 #define USONIC_IN A4
 #define PIR_R_IN  22
 
-void setup() {
-    pinMode(DMX_OUT, OUTPUT);    // send DMX
-    pinMode(PIR_R_OUT, OUTPUT);  // announce PIR R signal
-    pinMode(USONIC_OUT, OUTPUT); // announce USONIC signal
-    pinMode(PIR_L_OUT, OUTPUT);  // announce PIR L signal
-    pinMode(PIR_L_IN, INPUT);    // read PIR L
-    pinMode(PIR_R_IN, INPUT);    // read PIR R
-    Serial.begin(115200);
-    DmxSimple.usePin(6);
-    DmxSimple.maxChannel(100);
-}
+#define STATE_BASELINE 0
+#define STATE_APPROACH 1
+#define STATE_STORY    2
+#define STATE_LEAVING  3
+
+#define APPROACH_TIMEOUT 30000
+#define LEAVING_TIMEOUT  30000
 
 void setLightBar(int address, int r, int g, int b) {
     DmxSimple.write(address, 255);   // master dimmer, 0-255 -> dim-bright
@@ -53,16 +52,90 @@ void setLightBar(int address, int r, int g, int b) {
     DmxSimple.write(address + 6, b); // blue channel, 0-255 -> dim-bright
 }
 
-void loop() {
-    int i;
-
-    int sensorValue = analogRead(USONIC_IN);
-    if(sensorValue < USONIC_THRESHOLD) {
-        digitalWrite(USONIC_OUT, HIGH);
+int motionDetected(void) {
+    if(digitalRead(PIR_L_IN) | digitalRead(PIR_R_IN)) {
+        return TRUE;
     } else {
-        digitalWrite(USONIC_OUT, LOW);
+        return FALSE;
     }
-    Serial.println(sensorValue);
-    delay(100);
-    setLightBar(THE_ONLY_BAR_WE_HAVE, 255 - min(sensorValue, 255), 0, min(sensorValue, 255));
+}
+
+int personBetweenMirrors(void) {
+    if(analogRead(USONIC_IN)) { // will get more complicated with 4 sensors
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
+void updateStateMachine(void) {
+    static unsigned long approach_timer;
+    static unsigned long leaving_timer;
+
+    switch(state) {
+        case STATE_BASELINE:
+            // set edge light to low
+            // set ambient sound to very low
+            // stop any story
+            if(motionDetected()) {
+                state = STATE_APPROACH;
+                approach_timer = millis();
+            }
+            break;
+        case STATE_APPROACH:
+            // play alert
+            // set ambient sound to high
+            // single pulse of edge light, then down to medium
+            if(personBetweenMirrors()) {
+                state = STATE_STORY;
+            } else if(approach_timer - millis() > APPROACH_TIMEOUT) {
+                state = STATE_BASELINE;
+            }
+            break;
+        case STATE_STORY:
+            // play start sound, then play next story as long as we're in this state
+            // set ambient sound to low
+            // set edge light to high
+            if(!personBetweenMirrors()) {
+                if(motionDetected()) {
+                    state = STATE_LEAVING;
+                    leaving_timer = millis();
+                } else {
+                    // need a timeout in here?
+                    state = STATE_BASELINE; // this is the case where the person vanished without tripping the PIR
+                }
+            }
+            break;
+        case STATE_LEAVING:
+            // play leaving sound
+            // set ambient sound to high
+            // set edge light to medium
+            // fade story to 0
+            if(personBetweenMirrors()) { // ah, they went back in!
+                state = STATE_STORY;
+            }
+            if(leaving_timer - millis() > LEAVING_TIMEOUT) {
+                state = STATE_BASELINE;
+            }
+            break;
+    }
+}
+
+void setup() {
+    pinMode(DMX_OUT, OUTPUT);    // send DMX
+    pinMode(PIR_R_OUT, OUTPUT);  // announce PIR R signal
+    pinMode(USONIC_OUT, OUTPUT); // announce USONIC signal
+    pinMode(PIR_L_OUT, OUTPUT);  // announce PIR L signal
+    pinMode(PIR_L_IN, INPUT);    // read PIR L
+    pinMode(PIR_R_IN, INPUT);    // read PIR R
+    Serial.begin(115200);
+    DmxSimple.usePin(6);
+    DmxSimple.maxChannel(100);
+
+    // need to turn on back light here
+}
+
+void loop() {
+    static int state = STATE_BASELINE;
+    updateStateMachine();
 }
